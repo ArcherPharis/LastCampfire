365cb6b026a23f056daa46ba263312c3de1c9272
Ramos <roramos6@student.uiwtx.edu>
2021-09-21 15:27:50 -0500

    input read in Movement variable

--- Assets/Prefabs/Player.cs
+++ Assets/Prefabs/Player.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    InputActions inputActions;
+    Vector2 moveInput;
+
+    private void Awake()
+    {
+        inputActions = new InputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
     // Start is called before the first frame update
     void Start()
     {
+        inputActions.Gameplay.Move.performed += MoveInputsUpdated;
+        inputActions.Gameplay.Move.canceled += MoveInputsUpdated;
         
+    }
+
+    void MoveInputsUpdated(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>(); // what is the value?
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log($"player input is:{moveInput}");
     }
 }
